3164011f119e6bcc0fa08c3a4b53b761521a86da
isalvama <inesalvans@gmail.com>
2026-02-20 17:43:33 +0100

    feat: add Java Reflection code to test if @ToJsonFile annotation is running at compile time

--- src/main/java/second_level/Main.java
+++ src/main/java/second_level/Main.java
@@ -1,8 +1,25 @@
 package second_level;
 
+import java.lang.annotation.Annotation;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         OnlineWorker worker = new OnlineWorker("Ines", "Salvans", 20, 10);
         JsonFileSaver.saveObjectInJsonFile(worker);
+
+        try{
+            Class aClass = OnlineWorker.class;
+            Annotation[] annotations = aClass.getAnnotations();
+
+            for (Annotation annotation : annotations){
+                if (annotation instanceof ToJsonFile){
+                    ToJsonFile toJsonAnnotation = (ToJsonFile) annotation;
+                    System.out.println("name: " + toJsonAnnotation.annotationType());
+                    System.out.println("value: " + toJsonAnnotation.toString());
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Error: annotation not found");
+        }
     }
 }
